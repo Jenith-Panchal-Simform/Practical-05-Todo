5dc0975db888efce0fbc6bfd49be8eb2e8df99ac
Jenith-Panchal-Simform <jenith.panchal@simformsolutions.com>
2026-06-18 11:14:32 +0530

    feat:add axios interceptor

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
+import { Provider } from "react-redux";
 import { Header } from "./components/Header";
 import { Todos } from "./features/todos/components/Todos";
 import { ThemeProvider } from "./features/todos/context/ThemeProvider";
+import { store } from "./features/todos/store/store";
+
 
 function App() {
   return (
     <ThemeProvider>
       <div className="h-screen flex flex-col overflow-hidden">
         <Header />
-        <Todos />
+        <Provider store={store}>
+          <Todos />
+        </Provider>
       </div>
     </ThemeProvider>
   );
