a406ca5a4d021f72e465dcd8f4bec5cab66b53c7
Jenith-Panchal-Simform <jenith.panchal@simformsolutions.com>
2026-06-23 10:43:58 +0530

    refactor:wrap entire app in Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
-import { Provider } from "react-redux";
-
 import { Header } from "./components/Header";
 import { Todos } from "./features/todos/components/Todos";
 import { ThemeProvider } from "./features/todos/context/ThemeProvider";
-import { store } from "./features/todos/store/store";
 
 function App() {
   return (
     <ThemeProvider>
       <div className="h-screen flex flex-col overflow-hidden">
         <Header />
-        <Provider store={store}>
-          <Todos />
-        </Provider>
+        <Todos />
       </div>
     </ThemeProvider>
   );
